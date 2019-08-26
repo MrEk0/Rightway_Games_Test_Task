4be9e10cd11d5a802447efc5cb84c626e4487715
MrEk0 <51297787+MrEk0@users.noreply.github.com>
2019-08-26 20:49:27 +0300

    asteroid Script Objects were added

---
+++ Rightway_Games/Assets/Scripts/Asteroid.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="New Asteroid", menuName ="Asteroid/Create a new asteroid")]
+public class Asteroid : ScriptableObject
+{
+    [SerializeField] float strength;
+    [SerializeField] Sprite sprite;
+    [SerializeField] float speed;
+    [SerializeField] float size;
+
+    public float GetStrength()
+    {
+        return strength;
+    }
+    public Sprite GetSprite()
+    {
+        return sprite;
+    }
+    public float GetSpeed()
+    {
+        return speed;
+    }
+    public float GetSize()
+    {
+        return size;
+    }
+}
