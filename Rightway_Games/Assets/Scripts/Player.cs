1dbb88835ded5ac831cf1e564279bef774495040
MrEk0 <51297787+MrEk0@users.noreply.github.com>
2019-08-26 23:14:45 +0300

    player movement

---
+++ Rightway_Games/Assets/Scripts/Player.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float speed;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizPosition = Input.GetAxis("Horizontal");
+        float Xpos = transform.position.x + horizPosition * speed * Time.deltaTime;
+        transform.position = new Vector2(Xpos, transform.position.y);
+    }
+}
