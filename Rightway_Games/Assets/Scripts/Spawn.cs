12364435e6693948d134b5951ff3649dffe9ea82
MrEk0 <51297787+MrEk0@users.noreply.github.com>
2019-08-27 17:48:53 +0300

    spawn asteroids

---
+++ Rightway_Games/Assets/Scripts/Spawn.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+    [SerializeField] List<GameObject> prefabsToSpawn;
+    [SerializeField] float timeBetweenSpawns;
+
+    float timeSinceLastSpawn = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timeSinceLastSpawn>timeBetweenSpawns)
+        {
+            SpawnObjects();
+            timeSinceLastSpawn = 0f;
+        }
+        timeSinceLastSpawn += Time.deltaTime;
+    }
+
+    private void SpawnObjects()
+    {
+        int randomObjectIndex = Random.Range(0, prefabsToSpawn.Count);
+
+        Instantiate(prefabsToSpawn[randomObjectIndex], transform.position, transform.rotation);
+    }
+}
