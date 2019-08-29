f1677d42c80d7c3d4771db8653288c5be76dc802
MrEk0 <51297787+MrEk0@users.noreply.github.com>
2019-08-29 13:11:05 +0300

    enemy ship scrip obj
    created a couple of enemy ship by using scriptable object

---
+++ Rightway_Games/Assets/Scripts/Enemies/Enemy.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="New enemy ship", menuName ="Enemy ship")]
+public class Enemy :ScriptableObject
+{
+    [SerializeField] float strength;
+    [SerializeField] Sprite sprite;
+    
+}
