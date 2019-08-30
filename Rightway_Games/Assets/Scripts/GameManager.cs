680bf7b6c506973e0e47ecf6e05dd5d6366bffaa
MrEk0 <51297787+MrEk0@users.noreply.github.com>
2019-08-30 14:52:35 +0300

    pause button
    show pause menu when pause the
    button has been clicked

---
+++ Rightway_Games/Assets/Scripts/GameManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] GameObject PausePrefab;
+
+    GameObject canvas;
+    GameObject pauseMenu;
+
+    //private void Awake()
+    //{
+    //    if(SceneManager.GetActiveScene().buildIndex==1)
+    //    canvas = GameObject.Find("Canvas");
+    //}
+
+    //private void Update()
+    //{
+    //    Debug.Log(canvas);
+    //}
+
+    public void Pause()
+    {
+        //show window
+        //if (SceneManager.GetActiveScene().buildIndex == 1)
+            canvas = GameObject.Find("Canvas");
+        pauseMenu =Instantiate(PausePrefab, canvas.transform);
+        Time.timeScale = 0;
+    }
+
+    public void Resume()
+    {
+        //close window
+        Destroy(pauseMenu);
+        Time.timeScale = 1;
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
